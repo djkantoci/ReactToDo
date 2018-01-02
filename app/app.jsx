92b0d65244a2af442c6490bf2fb2c9b9dd1c42c5
Durdica Kantoci Buric <durdica.kantoci.buric@ecx.io>
2018-01-02 19:37:56 +0100

    Refactor TodoList Todo and AddTodo to work with redux

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
@@ -12,7 +13,7 @@ store.subscribe(() => {
 });
 
 store.dispatch(actions.addTodo('Clean the house'));
-store.dispatch(actions.addTodo('Wash car'));
+//store.dispatch(actions.addTodo('Wash car'));
 store.dispatch(actions.setSearchText('the'));
 store.dispatch(actions.toggleShowCompleted());
 
@@ -24,6 +25,8 @@ $(document).foundation();
 require('style-loader!css-loader!sass-loader!applicationStyles');
 
 ReactDOM.render(
-  <TodoApp/>,
+  <Provider store={store}>
+      <TodoApp/>
+  </Provider>,
   document.getElementById('app')
 );
